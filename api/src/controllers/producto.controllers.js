e3ec1c2380fa989631c80c72f75fb08f143a7444
leandro <rojasleandroismael617@gmail.com>
2021-06-01 15:13:14 -0300

    sacando codigo comentado

--- api/src/controllers/producto.controllers.js
+++ api/src/controllers/producto.controllers.js
@@ -33,59 +33,7 @@ const createProduct = async (req = Request, res = Response) => {
   }
 };
 
-// const getPost = async (req = Request, res = Response) => {
-//   const post = await repository.getPost(req.params.id);
-//   if (post === null) {
-//     res.status(404).send("not Found");
-//   } else {
-//     res.json(post);
-//   }
-// };
 
-// const updatePost = async (req = Request, res = Response) => {
-//   const { title, content, image, category } = req.body;
-//   const postToUpdate = await sequelize.Post.findByPk(req.params.id);
-//   if (postToUpdate === null) {
-//     res.status(404).send("post Not Found");
-//   } else {
-//     if (!image || (image && image.match(/([a-z\-_0-9\/\:\.]*\.(jpg|jpeg|png|gif))/i))) {
-//       const result = await sequelize.Post.update(
-//         {
-//           title: title,
-//           content: content,
-//           image: image,
-//           cat_id: category,
-//         },
-//         {
-//           where: {
-//             id: req.params.id,
-//           },
-//         },
-//       );
-//       res.status(200).json(result);
-//     } else {
-//       res.status(500).send("image field must be a image url format");
-//     }
-//   }
-// };
-
-// const deletePost = async (req = Request, res = Response) => {
-//   const postToDelete = await sequelize.Post.findByPk(req.params.id);
-//   if (postToDelete === null) {
-//     res.status(404).send("post Not Found");
-//   } else {
-//     sequelize.Post.destroy({
-//       where: {
-//         id: req.params.id,
-//       },
-//     }).then((result, error) => {
-//       if (error) {
-//         throw error;
-//       }
-//       res.status(200).json(result);
-//     });
-//   }
-// };
 
 module.exports = {
   createProduct,
